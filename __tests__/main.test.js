7bd348f41b70e0ba35d7948521ada69e9345be53
Louise <emmalouisekarlsson@hotmail.com>
2024-10-01 11:12:28 +0200

    Formaterat

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -3,12 +3,12 @@ const changeText = require('../main');
 
 describe("Mina test", () => {
 
-test("Change text to uppercase 1", () => {
-    
-    expect(changeText("välkommen")).toBe("VÄLKOMMEN");
-});
-test("Change text to uppercase 2", () => {
-    
-    expect(changeText("det var så lite")).toBe("det var så LITE");
-});
+    test("Change text to uppercase 1", () => {
+
+        expect(changeText("välkommen")).toBe("VÄLKOMMEN");
+    });
+    test("Change text to uppercase 2", () => {
+
+        expect(changeText("det var så lite")).toBe("det var så LITE");
+    });
 });
